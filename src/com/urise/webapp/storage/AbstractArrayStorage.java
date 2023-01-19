398a927e27d809a612bec5b972dc5a728b225cb6
Dima <bad123457@gmail.com>
2023-01-19 23:07:44 +0100

    lesson_3_edits

--- src/com/urise/webapp/storage/AbstractArrayStorage.java
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -2,12 +2,10 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
-/**
- * Array based storage for Resumes
- */
-public abstract class AbstractArrayStorage implements Storage {
+import java.util.Arrays;
 
-    private static final int STORAGE_LIMIT = 10000;
+public abstract class AbstractArrayStorage implements Storage {
+    protected static final int STORAGE_LIMIT = 10000;
 
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
@@ -24,5 +22,53 @@ public abstract class AbstractArrayStorage implements Storage {
         }
         return storage[index];
     }
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("Resume " + r.getUuid() + " not exist");
+        } else {
+            storage[index] = r;
+        }
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
+    }
+
+    public void save(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index >= 0) {
+            System.out.println("Resume " + r.getUuid() + " already exist");
+        } else if (size >= STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
+        } else {
+            saveByIndex(r, index);
+            size++;
+        }
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " not exist");
+        } else {
+            deleteByIndex(index);
+            storage[size - 1] = null;
+            size--;
+        }
+    }
+
+    protected abstract void saveByIndex(Resume r, int index);
+
+    protected abstract void deleteByIndex(int index);
+
+    public abstract int getIndex(String uuid);
+
 
 }
--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -1,4 +1,28 @@
 package com.urise.webapp.storage;
 
-public class SortedArrayStorage {
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
+public class SortedArrayStorage extends AbstractArrayStorage {
+
+
+    @Override
+    protected void saveByIndex(Resume r, int index) {
+        int insertionIndex = -index - 1;
+        System.arraycopy(storage, insertionIndex, storage, -index, size - insertionIndex);
+        storage[insertionIndex] = r;
+    }
+
+    @Override
+    protected void deleteByIndex(int index) {
+        System.arraycopy(storage, index + 1, storage, index, size - index - 1);
+    }
+
+    @Override
+    public int getIndex(String uuid) {
+        Resume searchKey = new Resume();
+        searchKey.setUuid(uuid);
+        return Arrays.binarySearch(storage, 0, size, searchKey);
+    }
 }
